cdb5bea34b1c0cc2131d64ffd3d63d818337b765
Kelly <kellybhoward@gmail.com>
2016-01-15 13:13:03 -0800

    add favicon links

--- application/views/partials/meta.php
+++ application/views/partials/meta.php
@@ -6,7 +6,8 @@
 <meta name="description" content="Red Belt Reviewer with Book Reviews"/>
 
 <link href="/assets/stylesheets/css/bootstrap.min.css" rel="stylesheet"/>
-
+<link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/assets/imagesfavicon.ico" type="image/x-icon">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="/assets/js/validations.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
